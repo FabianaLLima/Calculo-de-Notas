135e707b126c0cf281df0216e7155754d5a7d179
Fabiana Limma <fabianallima@hotmail.com>
2023-02-15 00:13:04 -0300

    Playground calculo de notas

---
+++ MyPlayground calculo de notas.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let testGradeOne = 5
+let testGradeTwo = 10
+let testGradeThree = 3
+let tesGradeFor = 8
+
+func somaNotas() -> Double {
+    let soma = testGradeOne + testGradeTwo + testGradeThree + tesGradeFor
+    return soma/4
+ }
+somaNotas()
+
+// tranformar os inteiros em double é so colocaar.algum número
+
