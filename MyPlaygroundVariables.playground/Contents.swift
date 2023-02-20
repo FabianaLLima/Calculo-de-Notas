55af24b4ec1374ddcb08f157c673e873558be99c
Fabiana Limma <fabianallima@hotmail.com>
2023-02-20 18:45:22 -0300

    Changing variable values

---
+++ MyPlaygroundVariables.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var a = 10
+var b = 20
+let temp = a
+
+a = b
+b = temp
+
+print("new value from A: \(a)")
+print("new value from B: \(b)")
+
+//Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B.
+//A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o
+//valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados
+//nas variáveis.
