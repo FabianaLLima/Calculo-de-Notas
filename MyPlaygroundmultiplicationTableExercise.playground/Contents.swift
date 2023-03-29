5bbbdd27ade8c658fd4fca9d057ebbcf76612b42
Fabiana Limma <fabianallima@hotmail.com>
2023-03-29 19:27:16 -0300

    repetition structure exercise

---
+++ MyPlaygroundmultiplicationTableExercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Fazer exercício de lógica Faça um programa que mostre as tabuadas dos números de 1 a 10 usando laços de repetição.
+
+func tabuada() {
+    for multi in 1...10 {
+       for multi2 in 1...10 {
+      print("\(multi) * \(multi2) = ")
+        
+       }
+    }
+}
+tabuada()
