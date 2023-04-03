6d52d9211c642f84e2ae9b75e3bfcc96c4fffba4
Fabiana Limma <fabianallima@hotmail.com>
2023-04-03 17:22:31 -0300

    another exercise trying to improve logic

---
+++ MyPlaygroundCalculationExerciseMonthsAndDays.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//Considerando que todos os meses tenham 30 dias, calcular o total de dias de n meses.
+
+func calculateDays(month: Int, days: Int) {
+    print(days*month)
+}
+
+calculateDays(month: 12, days: 30)
