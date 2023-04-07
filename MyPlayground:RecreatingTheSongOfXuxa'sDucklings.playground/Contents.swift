faebc8bdbe97df3e9bc888b6044c67671c9e6d83
Fabiana Limma <fabianallima@hotmail.com>
2023-04-07 17:03:27 -0300

    exercício de lógica diferente para se divertir

---
+++ MyPlayground:RecreatingTheSongOfXuxa'sDucklings.playground/Contents.swift
@@ -0,0 +1,12 @@
+//O objetivo desse exercício é recriar a música da xuxa dos patinhos utilizando estrutura de repetição(e se divertir também ;))
+
+var duck = 5
+while duck != 0 {
+    print("\(duck) patinhos foram passear. além das montanha para brincar. A mamãe gritou: Quá, quá, quá, quá mas só \(duck - 1) patinhos voltaram de lá.")
+    duck = duck - 1
+}
+
+duck = 5
+print("A mamãe patinha foi procurar Além das montanhas Na beira do mar A mamãe gritou: Quá, quá, quá, quá e \(duck) patinhos voltaram de lá.")
+
+
